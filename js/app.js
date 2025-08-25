71182e53a0c056bd85eeb353055d21099140a5a1
MayerlyZ <mayerlyzapatarodriguez@gmail.com>
2025-08-25 09:58:07 -0500

    cambio de imagemes

--- js/app.js
+++ js/app.js
@@ -1,37 +1,46 @@
-const openNav = document.querySelector(".hamburger");
-const closeNav = document.querySelector(".nav__close");
-const nav = document.querySelector(".nav__list");
-
-openNav.addEventListener("click", () => {
-  const navLeft = nav.getBoundingClientRect().left;
-  if (navLeft < 0) {
-    nav.style.left = "0";
-    document.body.classList.add("active");
-  }
-});
-
-closeNav.addEventListener("click", () => {
-  const navLeft = nav.getBoundingClientRect().left;
-  if (navLeft === 0) {
-    nav.style.left = "-40rem";
-    document.body.classList.remove("active");
-  }
-});
-
-// Smooth Scroll
-const scrollLinks = [...document.querySelectorAll(".scroll-link")];
-
-scrollLinks.forEach(link => {
-  link.addEventListener("click", e => {
-    const id = e.target.getAttribute("href").slice(1);
-    const element = document.getElementById(id);
-    const position = element.offsetTop;
-
-    window.scrollTo({
-      left: 0,
-      top: position,
+$(document).ready(function() {
+    $('#adaptive').lightSlider({
+        adaptiveHeight:true,
+        auto:true,
+        item:1,
+        slideMargin:0,
+        loop:true
     });
-    nav.style.left = "-40rem";
-    document.body.classList.remove("active");
-  });
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function(){
+            $('#autoWidth').removeClass('cS-hidden');
+        }
+    });
+
+    // Lógica de los formularios y la barra de búsqueda
+    $(document).on('click', '.search-icon', function(){
+        $('.search-bar').removeClass('hidden').addClass('flex');
+    });
+
+    $(document).on('click', '.search-cancel-icon', function(){
+        $('.search-bar').addClass('hidden').removeClass('flex');
+    });
+
+    $(document).on('click', '.user-icon, .already-account-btn', function(){
+        $('.form-container').removeClass('hidden').addClass('flex');
+        $('.login-form').removeClass('hidden').addClass('flex');
+        $('.sign-up-form').addClass('hidden').removeClass('flex');
+    });
+
+    $(document).on('click', '.sign-up-btn', function(){
+        $('.form-container').removeClass('hidden').addClass('flex');
+        $('.sign-up-form').removeClass('hidden').addClass('flex');
+        $('.login-form').addClass('hidden').removeClass('flex');
+    });
+
+    $(document).on('click', '.form-cancel-icon', function(){
+        $('.form-container').addClass('hidden').removeClass('flex');
+    });
+    
+    // Lógica del menú responsivo
+    $(document).on('click', '.menu-toggle', function() {
+        $('.mobile-menu').toggleClass('hidden');
+    });
 });
